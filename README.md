e46c22dd5bfb5edbfc7f45c4d02941ce944aecb6
Alexander Kabolov <akabolov@Alexanders-MacBook-Pro.local>
2023-08-25 23:34:33 +0200

    feat: added logger, storage and configs

---
+++ README.md
@@ -0,0 +1,3 @@
+# Url-shortener
+
+written in go
---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,89 @@
+package sqlite
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"url-shortener/internal/storage"
+
+	"github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+// ? why we cant return just storage itself and not pointer to storage
+// A in other case we would need to return empty structs instead of nil
+func New(storagePath string) (*Storage, error) {
+	// op is function name
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	stmt, err := db.Prepare(`
+		CREATE TABLE IF NOT EXISTS url(
+			id INTEGER PRIMARY KEY,
+			alias TEXT NOT NULL UNIQUE,
+			url TEXT NOT NULL);
+		CREATE INDEX IF NOT EXISTS idx_alias on url(alias);
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, err
+}
+
+func (s *Storage) SaveUrl(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveUrl"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) GetUrl(alias string) (string, error) {
+	const op = "storage.sqlite.GetUrl"
+
+	qry, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+
+	var resUrl string
+	err = qry.QueryRow(alias).Scan(&resUrl)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", storage.ErrURLNotFound
+		}
+	}
+
+	return resUrl, nil
+}
+
+//func (s *Storage) DeleteUrl(alias string) error
